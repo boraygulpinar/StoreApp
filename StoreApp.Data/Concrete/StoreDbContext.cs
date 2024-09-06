11ce0f9c7b2a2c52ca4158712f38467c6888f4f4
Boray Gulpinar <boray112@gmail.com>
2024-09-06 05:34:03 +0300

    Add project files.

---
+++ StoreApp.Data/Concrete/StoreDbContext.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace StoreApp.Data.Concrete
+{
+    public class StoreDbContext : DbContext
+    {
+        public StoreDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products => Set<Product>();
+    }
+}
